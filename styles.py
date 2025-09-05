6c5486da217e116f6a0da23a14c95255a352d5c6
Vishal Murali <vishalm@vortexflow.org>
2025-09-05 17:00:23 +0530

    Delete styles

--- styles.py
+++
@@ -1,9 +0,0 @@
-
-REFERENCE_STYLES = {
-    "harvard": "{author} ({year}). {title}. {journal}, {publisher}.",
-    "apa": "{author} ({year}). {title}. {journal}. {publisher}.",
-    "mla": "{author}. \"{title}.\" {journal}, {year}, {publisher}.",
-    "chicago": "{author}. {title}. {journal}. {publisher}, {year}.",
-    "vancouver": "{author}. {title}. {journal}. {year}; {publisher}.",
-    "ieee": "{author}, \"{title},\" {journal}, {publisher}, {year}."
-}
